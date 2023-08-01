01c53c98a341f8dc0f41b020c530fd6aaccec7fb
solfany <solfany1999@gmail.com>
2023-08-01 21:07:24 +0900

    Initial commit

---
+++ src/main/java/com/example/springbootloginproject/SpringBootLoginProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootloginproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoginProjectApplication.class, args);
+    }
+
+}
